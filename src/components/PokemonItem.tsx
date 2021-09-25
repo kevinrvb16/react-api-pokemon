e06dbedcb4c0590f40ee88b0dbbd8250a4ca1395
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-09-25 13:44:18 -0300

    feat: create PokemonItem component and pages and components folders

---
+++ src/components/PokemonItem.tsx
@@ -0,0 +1,9 @@
+function PokemonItem() {
+    return (
+        <div>
+            <h2>Nome do Pokémon</h2>
+        </div>
+    )
+}
+
+export default PokemonItem
---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import PokemonItem from '../components/PokemonItem'
+
+function App() {
+  return (
+    <div>
+      <h1>Meu site com react</h1>
+      <PokemonItem/>
+      <PokemonItem/>
+      <PokemonItem/>
+      <PokemonItem/>
+    </div>
+  );
+}
+
+export default App;
