deaf11df576cc4ef7a1c9560be17d66db8fe6dc1
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-09-26 11:05:22 -0300

    feat(home): pokemons.map integrated with api response

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,20 +23,16 @@ function Home() {
 
     setPokemons(data.results)
   }
-  console.log({ pokemons })
-
+  
   return (
     <div className={styles.container}>
       <img src={PokemonLogo}
         alt="Pokemon Logo"
         onClick={() => getAllPokemons()}
       />
-      <PokemonItem name="pikachu" />
-      <PokemonItem name="charmander" />
-      <PokemonItem name="bulbasaur" />
-      <PokemonItem name="ditto" />
-      <PokemonItem name="delibird" />
-      <PokemonItem name="mantine" />
+      {pokemons?.map((pokemon) => (
+        <PokemonItem name={pokemon.name} key={pokemon.name} />
+      ))}
     </div>
   );
 }
