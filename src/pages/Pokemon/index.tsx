ee8aeae4410b9c860f48158d5fb722f852f35eaf
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-09-27 21:42:26 -0300

    feat(Pokemon): create IPokemon interface

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -3,15 +3,25 @@ import { useParams } from 'react-router'
 import api from '../../services/api'
 import styles from './styles.module.css'
 
+interface IPokemon {
+    height: number;
+    name: string;
+    weight: number;
+    types: { type: {name: string} }[]
+    abilities: {ability: { name: string}}[]
+    sprites: {other: {'official-artwork': {front_default: string}}}
+}
+
 interface IParams {
     pokemon: string
 }
+
 const Pokemon: React.FC = () => {
-    const [pokemon, setPokemon] = useState<any>()
+    const [pokemon, setPokemon] = useState<IPokemon>()
     const { pokemon: pokemonName } = useParams<IParams>()
 
     const getPokemonInfo = async () => {
-        const {data} = await api.get(`pokemon/${pokemonName}`)
+        const {data} = await api.get<IPokemon>(`pokemon/${pokemonName}`)
 
         setPokemon(data)
     }
