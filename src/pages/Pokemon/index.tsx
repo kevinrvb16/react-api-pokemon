28a37bb6d6f7377455f0a6a7764d3b16763bc3b8
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-09-28 16:07:05 -0300

    feat(Pokemon): page style

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
 import api from '../../services/api'
 import styles from './styles.module.css'
 
@@ -31,12 +32,15 @@ const Pokemon: React.FC = () => {
     }, [])
     return (
         <div className={styles.container}>
-        <h1>{pokemon?.name}</h1>
+        <h1 className={styles.title}>{pokemon?.name}</h1>
         <img src={pokemon?.sprites.other['official-artwork'].front_default} alt=""></img>
-        <h2>Altura: {pokemon?.height}</h2>
-        <h2>Peso: {pokemon?.weight}</h2>
-        <h2>Tipos: {pokemon?.types.map((type) => type.type.name).join(', ')}</h2>
-        <h2>Habilidades: {pokemon?.abilities.map((ability) => ability.ability.name).join(', ')}</h2>
+        <h2 className={styles.card}>Altura: {pokemon?.height}</h2>
+        <h2 className={styles.card}>Peso: {pokemon?.weight}</h2>
+        <h2 className={styles.card}>Tipos: {pokemon?.types.map((type) => type.type.name).join(', ')}</h2>
+        <h2 className={styles.card}>Habilidades: {pokemon?.abilities.map((ability) => ability.ability.name).join(', ')}</h2>
+        <Link to="/">
+            <button className={styles.button}>Voltar</button>
+        </Link>
         </div>
     )
 }
